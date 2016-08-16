42063beb6c3261b3900a44c1fc5d92c9540e447a
Spencer <Fisher.spencer@gmail.com>
2016-08-15 18:11:06 -0700

    Made changes idk

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -1,120 +1,128 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-//using System.Math;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
 public class Node : MonoBehaviour {
-    public int numFrames = 0;
-    public int numBonds = 0;
-    private List<Object> Bonds = new List<Object>();
-    public int openBonds;
+    public float radius = 5;
+    public float dragVel = 1.0f;
+    public float frozeSpeed = .01f;
+    public int isFrozen = 0;
+    public bool DebugOn = false;
+    public bool Meshed = false;
+    public List<Vector3> Local = new List<Vector3>();
     void FixedUpdate()
     {
-        //get other nodes within range (2units)
-        float radius = 1, thrust, dist;
-        openBonds = numBonds - Bonds.Count;
+        if (Meshed)
+            Meshed = false;
+        if (isFrozen<100)
+        {
+            //get other nodes within range (2units)
+            float thrust, dist;
+            Vector3 temp;
+            Vector3 source = transform.position;
+            Vector3 dest;
+            Vector3 vec;
+            Rigidbody body = GetComponent<Rigidbody>();
+
+            //check frozen
+            if (body.velocity.magnitude < frozeSpeed)
+            {
+                isFrozen++;
+            }
+            else 
+            {
+                isFrozen = 0;
+            }
+            
+            if(isFrozen >= 100)
+            {
+                body.velocity = Vector3.zero;
+            }
+            else
+            {
+                Local = new List<Vector3>();
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 1.5f * radius);
+                foreach (Collider hit in colliders)
+                {
+                    Rigidbody rb = hit.GetComponent<Rigidbody>();
+                    if (rb != null)
+                    {
+                        dest = hit.transform.position;
+                        vec = source - dest;
+                        dist = vec.magnitude;
+                        Local.Add(-vec);
+                        if (dist > 0)
+                        {
+                            if (dist > radius)
+                            {
+                                thrust = (float)(1 - System.Math.Pow(((dist - 1.25 * radius) * (4/radius)), 2)); //attract
+                                temp = (body.velocity - rb.velocity);
+                                temp *= -1f;
+                                temp *= (float)(dragVel / (temp.magnitude * 10));
+                                body.AddForce(temp);
+
+                                //body.velocity = (body.velocity+temp)*.5f; //normalize vel
+                                //rb.velocity -= (temp * (float)(dragVel / 100.0)); //normalize vel
+                            }
+                            else
+                            {
+                                thrust = (float)((1 / (dist * dist)) - (1 / (radius * radius))) * -100f; //repel
+                                //thrust = -100;
+                            }
+
+                            body.AddForce(vec * -thrust);
+                        }
+                    }
+                }
+            }
+        }
+        if ((GetComponent<Rigidbody>().velocity.magnitude > 0) & isFrozen >= 100)
+        {
+            if(DebugOn)
+                Debug.Log("vel = " + GetComponent<Rigidbody>().velocity.magnitude);
+            isFrozen = 0;
+        }
+
+    }
+    void Awake()
+    {
+        
+        float dist;
         Vector3 source = transform.position;
         Vector3 dest;
         Vector3 vec;
         Rigidbody body = GetComponent<Rigidbody>();
-
-        // create list and keep refs of bonded bros. check that first before doing sphere check.
-        // in sphere check see if they have an open slot before bonding with them.
-        // if bond passes both add as a bonded bro.
-        int i = 0;
-        int[] b = new int[Bonds.Count];
-        foreach(Collider a in Bonds)
+        if (body.transform.position == Vector3.zero)
         {
-            if (a != null)
-            {
-                //make sure they're still in range. if they're not delete ref
-                dest = a.transform.position;
-                vec = source - dest;
-                dist = vec.magnitude;
-                if (dist > radius * 2)
-                {
-                    b[i] = 0;
-                }
-                else
-                {
-                    //else add force
-
-                    Rigidbody rb = a.GetComponent<Rigidbody>();
-                    thrust = (float)(1 - System.Math.Pow(((dist - 1.5 * radius) * 2 * radius),2));
-                    rb.AddForce(vec * thrust);
-                    body.AddForce(vec * -thrust);
-                    b[i] = 1;
-                }
-                i++;
-            }
+            isFrozen = 100;
         }
-        for (i = -1; i >= 0; i--)
+        else
         {
-            if (b[i] == 0)
-            {
-                Bonds.RemoveAt(i);
-                openBonds++;
-            }
+
+            dest = Vector3.zero;//center of mass eh
+            vec = source - dest;
+            dist = vec.magnitude;
+            vec=(-vec*(1/dist));
+            vec *= 1.0f;
+            body.velocity = vec;
+            //Collider[] colliders = Physics.OverlapSphere(transform.position, 100);
+            //foreach (Collider hit in colliders)
+            //{
+            //    Rigidbody rb = hit.GetComponent<Rigidbody>();
+            //    dest = hit.transform.position;
+            //    vec = source - dest;
+            //    dist = vec.magnitude;
+            //    thrust = (float)(1 / (dist + 2.0));
+            //    thrust *= 5f;
+            //    if (rb != null & dist > 0)
+            //    {
+            //        body.AddForce(vec * -thrust);
+            //    }
+            //    //Debug.Log("impulse " + rb.impulse.magnitude);
+            //}
         }
-        
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 2 * radius);
-        foreach (Collider hit in colliders)
-        {
-            Rigidbody rb = hit.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                dest = hit.transform.position;
-                vec = source - dest;
-                dist = vec.magnitude;
-                if (dist > 0) // ie not self. unless some int f-ery is going on then some points might think they occupy the same space.
-                {
-
-                    thrust = (float)(System.Math.Pow(((dist - .5 * radius) * 2*radius), 2) - 1);
-                    //Debug.Log("dist: " + dist + " thrust: " + thrust);
-                    if (dist > radius)
-                        thrust = 0;
-
-                    if(openBonds>0)
-                    {
-                        // add bond if other node has open bond
-                        if (hit.GetComponent<Node>().openBonds > 0)
-                        {
-                            Debug.Log(hit.GetComponent<Node>().openBonds + ", " + openBonds);
-                            hit.GetComponent<Node>().openBonds -= 1;
-                            hit.GetComponent<Node>().Bonds.Add(GetComponent<Collider>());
-                            openBonds--;
-                            Bonds.Add(hit);
-                        }
-                    }
-                    rb.AddForce(vec * thrust);
-                    body.AddForce(vec * -thrust);
-                }
-            }
-            //Debug.Log("dist: " + dist+" thrust: "+thrust);
-
-        }
-        if (numFrames > 0 | numFrames < 0)
-        {
-            //Debug.Log(numFrames);
-            numFrames--;
-            colliders = Physics.OverlapSphere(transform.position, 100);
-            foreach (Collider hit in colliders)
-            {
-                Rigidbody rb = hit.GetComponent<Rigidbody>();
-                dest = hit.transform.position;
-                vec = source - dest;
-                dist = vec.magnitude;
-                thrust = (float)(1 / (dist + 2.0));
-                thrust *= 0.1f;
-                if (rb != null & dist > 0)
-                {
-                    rb.AddForce(vec * thrust);
-                    body.AddForce(vec * -thrust);
-                }
-
-            }
-        }
-        
     }
+    
 
 }
--- Assets/NodeList.cs
+++ Assets/NodeList.cs
@@ -2,34 +2,160 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
 public class NodeList : MonoBehaviour {
 
     // Use this for initialization
     public GameObject nodePrefab;
     public int numofnodes = 0;
-    private List<Object> Nodes = new List<Object>();
+    private List<GameObject> Nodes = new List<GameObject>();
+    //private Transform[] Nodes;
+    public Vector3 CenterOfMass = Vector3.zero;
+    public float Mass = 0;
+    public bool redrawMesh = false;
+    public List<Vector3> meshVert;
+    public List<int> meshTri;
+    private Mesh mesh;
 	void Start ()
     {
         float x, y, z;
-	    for (int i=0; i<numofnodes;i++)
+        GameObject a;
+
+        a = Instantiate(nodePrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        a.name = "Node" + Nodes.Count;
+        a.transform.parent = gameObject.transform;
+        Mass++;
+        //CenterOfMass += (a.transform.position - CenterOfMass) * (1 / (Mass));
+        Nodes.Add(a);
+	    for (int i=1; i<numofnodes;i++)
         {
-            x = Random.Range(-10.0f, 10.0f);
-            y = Random.Range(-10.0f, 10.0f);
-            z = Random.Range(-10.0f, 10.0f);
-            Nodes.Add(Instantiate(nodePrefab, new Vector3(x, y, z), Quaternion.identity));
+            x = Random.Range(-5.0f, 5.0f);
+            y = Random.Range(-5.0f, 5.0f);
+            z = Random.Range(-5.0f, 5.0f);
+            a = Instantiate(nodePrefab, new Vector3(x, y, z), Quaternion.identity) as GameObject;
+            a.transform.parent = gameObject.transform;
+            a.name = "Node" + Nodes.Count;
+            Mass++;
+            //CenterOfMass += (a.transform.position - CenterOfMass) * (1 / (Mass));
+            Nodes.Add(a);
 
         }
 	}
 
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
+            int num = 50;
             float x, y, z;
-            x = Random.Range(-5.0f, 5.0f);
-            y = Random.Range(-5.0f, 5.0f);
-            z = Random.Range(-5.0f, 5.0f);
-            Nodes.Add(Instantiate(nodePrefab, new Vector3(x, y, z), Quaternion.identity));
+            GameObject a;
+            for (int i = 0; i < num; i++)
+            {
+                x = Random.Range(-5.0f, 5.0f);
+                y = Random.Range(-5.0f, 5.0f);
+                z = Random.Range(-5.0f, 5.0f);
+                a = Instantiate(nodePrefab, new Vector3(x, y, z), Quaternion.identity) as GameObject;
+                a.transform.parent = gameObject.transform;
+                Mass++;
+                a.name = "Node" + Nodes.Count;
+                Nodes.Add(a);
+                redrawMesh = true;
+
+            }
         }
+        //calcmesh
+        //uhhhhhh needs to like. go through and find the 'surface' of the nodes 
+        //then, like, map those nodes to vertices. 
+
+        if(redrawMesh)
+        {
+            redrawMesh = false;
+            meshVert = new List<Vector3>();
+            int a = (int)Random.Range(0, Nodes.Count - 1);
+            Node child = Nodes[a].GetComponent<Node>() as Node;
+            FindVertNode(child);
+        }
+
+        // update mesh
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        GetComponent<MeshCollider>().sharedMesh = null;
+
+
+        mesh.name = "Procedural Mesh";
+        mesh.vertices = meshVert.ToArray();
+        mesh.triangles = meshTri.ToArray();
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+
+    }
+
+    
+    private void FindVertNode(Node curr)
+    {
+        List<Vector3> Vertices = new List<Vector3>();
+        curr.Meshed = true;
+        Vertices.Add(curr.transform.position);
+        List<Node> Local = new List<Node>();
+        List<int> VertRef = new List<int>();
+        Node a;
+        Vector3 normal = Vector3.zero;
+
+        // remove self from thigny. add self to current vert list
+        //do cast sphere in raduius for all those imediatley in contact , draw normal vector away from all dem.
+        Collider[] colliders = Physics.OverlapSphere(Vertices[0], curr.radius*1.1f);
+        foreach(Collider hit in colliders)
+        {
+            a = hit.GetComponent<Node>();
+            if(!a.Meshed)
+            {
+                //not added yet.
+                Local.Add(a);
+                Vertices.Add(a.transform.position);
+                VertRef.Add(-1);
+            }
+            //update normal vector 
+            normal -= a.transform.position;
+        }
+        // create list of verts. update with master list. check for repeats, update vert list to get from master
+        // getting older verts in master list if repeat
+
+        for(int i = 0; i<Vertices.Count-1;i++)
+        {
+            for(int j = 0; j<meshVert.Count-1; j++ )
+            {
+                if(Vertices[i]==meshVert[j]) // obviously not working correctly. dups in master list
+                {
+                    //ref already exist update thingy to do stuff
+                    VertRef[i] = j;
+                }
+
+            }
+            if(VertRef[i] == -1)
+            {
+                //if not found in master list
+                meshVert.Add(Vertices[i]);
+                VertRef[i] = meshVert.Count-1;
+            }
+        }
+
+        // use normal vector to start drawing tris (clockwise around the vec)
+        // merge tri list with master tri
+        for(int i=1;i<VertRef.Count-2;i++)
+        {
+            meshTri.Add(VertRef[0]);
+            meshTri.Add(VertRef[i]);
+            meshTri.Add(VertRef[i+1]);
+            //obviously this isn't picking them in the correct order but i'm just curious to see if i can draw something.
+        }
+
+        // pick nect vert to continue
+        foreach(Node node in Local)
+        {
+            FindVertNode(node);
+        }
+
+
     }
 }
